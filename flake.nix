7004e2bad55b5f46a807a67face8648c414f5dea
James Santucci <james.santucci@gmail.com>
2022-12-28 11:14:07 -0700

    output shells for different systems

--- flake.nix
+++ flake.nix
@@ -4,52 +4,64 @@
   inputs = {
     # follow a stable packages release
     nixpkgs.url = "nixpkgs/nixos-22.11";
+
   };
 
   outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-darwin";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    with pkgs;
 
     let
-      pythonDeps = [
-        python39
-        python39Packages.poetry
-        python39Packages.black
-      ];
-      javascriptDeps = [
-        nodejs-16_x
-        yarn
-        nodePackages.prettier
-        nodePackages.eslint
-      ];
-      javaDeps = [
-        openjdk11
-        (sbt.override { jre = openjdk11; })
-      ];
-    in
+      shellFor = system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        with pkgs;
 
+        let
+          pythonDeps = [
+            python39
+            python39Packages.poetry
+            python39Packages.black
+          ];
+          javascriptDeps = [
+            nodejs-16_x
+            yarn
+            nodePackages.prettier
+            nodePackages.eslint
+          ];
+          javaDeps = [
+            openjdk11
+            (sbt.override { jre = openjdk11; })
+          ];
+        in
+        {
+          name = system;
+          value = {
+            java = mkShell
+              {
+                packages = javaDeps;
+              };
+
+            python = mkShell {
+              packages = pythonDeps;
+            };
+
+            javascript = mkShell {
+              packages = javascriptDeps;
+            };
+
+            omnishell = mkShell {
+              packages = javascriptDeps ++ pythonDeps ++ javaDeps;
+            };
+          };
+        };
+    in
     {
-
-      devShells.${system} = {
-        java = mkShell {
-          packages = javaDeps;
-        };
-
-        python = mkShell {
-          packages = pythonDeps;
-        };
-
-        javascript = mkShell {
-          packages = javascriptDeps;
-        };
-
-        omnishell = mkShell {
-          packages = javascriptDeps ++ pythonDeps ++ javaDeps;
-        };
-      };
+      devShells = builtins.listToAttrs (map shellFor [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ]);
     };
 }
 
